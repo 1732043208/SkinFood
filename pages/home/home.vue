d3c1be9e00c90816450dd2304b8b92aae8f3d107
黄伟杰 <1732043208@qq.com>
2021-01-04 14:44:41 +0800

    commit 项目搭建完成

---
+++ pages/home/home.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view>
+			<text>haha</text>
+			<text>123</text>
+			<text>456</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
